b49d15349a71f625f7c1c89e85580c1ea3f1ad86
Sonix-Force <sonixforce1@gmail.com>
2021-11-16 18:20:21 -0600

    Avance en el proyecto CYP05

--- CYP05/CYP05/CYP05.cpp
+++ CYP05/CYP05/CYP05.cpp
@@ -1,5 +1,48 @@
 #include <stdio.h>
 
+#define CANTIDAD 2000
+
 int main() {
 
+	//Variables
+	int cantAlum, cuentAlum[CANTIDAD] = {}, tempCuent;
+	float califAlum[CANTIDAD] = {}, tempCalif;
+
+
+	//Detectar cantidad de alumnos, y con base a ello detectar poco a poco cada numero de cuenta y calificación.
+	scanf_s("%i", &cantAlum);
+
+	if (cantAlum != 0) {
+		for (int i = 0; i < cantAlum; i++) {
+			scanf_s("%i", &cuentAlum[i]);
+			scanf_s("%f", &califAlum[i]);
+		}
+
+		//Ordenar arreglos.
+
+		for (int i = 0; i < cantAlum; i++) {
+			for (int c = 0; c+1 <= i; c++) {
+				if (cuentAlum[c] == cuentAlum[c+1]) {
+					cuentAlum[c] = cuentAlum[c+1];
+				}
+				else if (cuentAlum[c] > cuentAlum[c+1]) {
+					tempCuent = cuentAlum[c+1];
+					cuentAlum[c+1] = cuentAlum[c];
+					cuentAlum[c] = tempCuent;
+				}
+			}
+		}
+
+
+		//Iprimir cantidad de alumnos, numeros de cuenta y calificaciones.
+		printf("%i\n", cantAlum);
+
+		for (int p = 0; p < cantAlum; p++) {
+			printf("%i ", cuentAlum[p]);
+			printf("%f\n", califAlum[p]);
+		}
+	}
+	else {
+		return 0;
+	}
 }
