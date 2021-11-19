0dbf728a64df5d178f66930e95357a8d6400847f
Sonix-Force <sonixforce1@gmail.com>
2021-11-18 23:20:18 -0600

    Cambios en CYP05

--- CYP05/CYP05/CYP05.cpp
+++ CYP05/CYP05/CYP05.cpp
@@ -9,7 +9,7 @@ int main() {
 	float califAlum[CANTIDAD] = {}, tempCalif;
 
 
-	//Detectar cantidad de alumnos, y con base a ello detectar poco a poco cada numero de cuenta y calificaciÃ³n.
+	//Detectar cantidad de alumnos, y con base a ello detectar poco a poco cada numero de cuenta y calificación.
 	scanf_s("%i", &cantAlum);
 
 	if (cantAlum != 0) {
@@ -19,21 +19,29 @@ int main() {
 		}
 
 		//Ordenar arreglos.
-
-		for (int i = 0; i < cantAlum; i++) {
-			for (int c = 0; c+1 <= i; c++) {
-				if (cuentAlum[c] == cuentAlum[c+1]) {
-					cuentAlum[c] = cuentAlum[c+1];
-				}
-				else if (cuentAlum[c] > cuentAlum[c+1]) {
-					tempCuent = cuentAlum[c+1];
-					cuentAlum[c+1] = cuentAlum[c];
+		for (int i = cantAlum; i != 0; i--) {
+			for (int c = 0; c + 1 <= i; c++) {
+				if (cuentAlum[c] > cuentAlum[c + 1] && cuentAlum[c + 1] != 0) {
+					tempCuent = cuentAlum[c + 1];
+					cuentAlum[c + 1] = cuentAlum[c];
 					cuentAlum[c] = tempCuent;
 				}
 			}
 		}
+		//Eliminar numeros de cuenta repetidos y dejar solamente la calificación más alta.
+		for (int i = cantAlum; i != 0; i--) {
+			for (int c = 0; c + 1 <= i; c++) {
+				for (int b = c; cuentAlum[b] == cuentAlum[b + 1]; b++) {
+					if (califAlum[b] > califAlum[b + 1]) {
+						califAlum[b + 1] = califAlum[b];
+						califAlum[b] = NULL;
+					}
+					cuentAlum[c] = NULL;
+				}
+			}
+		}
 
-
+		
 		//Iprimir cantidad de alumnos, numeros de cuenta y calificaciones.
 		printf("%i\n", cantAlum);
 
@@ -45,4 +53,5 @@ int main() {
 	else {
 		return 0;
 	}
+	return 0;
 }
